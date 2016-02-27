67d5e06e08126c80c7076cf1319b5af1d5275ca9
vagrant <vagrant@precise32.(none)>
2016-02-27 00:40:53 +0000

    Improving look and feel

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'haml'
 gem 'simple_form'
 gem "paperclip", "~> 4.3"
 gem 'devise'
+gem 'kaminari'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+	def check_if_image_is_uploaded post
+		if post.image_file_name != nil
+			post.image.url(:medium)
+		else
+			'placeholder.jpg'
+		end
+	end
 end
